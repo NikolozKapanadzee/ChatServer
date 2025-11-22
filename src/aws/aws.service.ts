42633202ed2fe739d31782ee8ac88d537f99b939
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-11-22 20:28:42 +0400

    Feat: AWS configuration finished

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -1,4 +1,19 @@
+import { S3Client } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AwsService {}
+export class AwsService {
+  private bucketName;
+  private s3;
+
+  constructor() {
+    this.bucketName = process.env.AWS_BUCKET_NAME;
+    this.s3 = new S3Client({
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      },
+      region: process.env.AWS_REGION,
+    });
+  }
+}
